1c7dd943ccfd83ddd23d050801aa5e219a44d7a7
jaredkitade <jkitade@cca.edu>
2021-09-07 15:04:05 -0700

    show nav class name

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -1,24 +1,30 @@
-// $(window).scroll(function(){
+$(document).ready(function(){
 
-//     var scroll = $(window).scrollTop();
 
-//     if (scroll >= 300) {
-//         $('.nav-bar').addClass('reveal');
-//     }
-//     else {
-//         $('.nav-bar').removeClass('reveal');
-//     }
+    // $(window).scroll(function(){
 
-//  });
+    //     var scroll = $(window).scrollTop();
 
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
+    //     if (scroll >= 300) {
+    //         $('.nav-bar').addClass('reveal');
+    //     }
+    //     else {
+    //         $('.nav-bar').removeClass('reveal');
+    //     }
 
-    if (scroll >= 200) {
-        $('.nav-bar').addClass('reveal');
-    }
-    // else {
-    //  $('.scroll-wrap').removeClass('scroll-fade');
-    // }
+    //  });
 
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 200) {
+            $('.nav-bar').addClass('reveal');
+        }
+        // else {
+        //  $('.scroll-wrap').removeClass('scroll-fade');
+        // }
+
+    });
+
+    
 });
