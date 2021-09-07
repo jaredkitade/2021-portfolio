60aac1435c954f869ff5900ab4216fb176586d01
jaredkitade <jkitade@cca.edu>
2021-09-07 15:10:41 -0700

    nav bar update

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -14,17 +14,24 @@ $(document).ready(function(){
 
     //  });
 
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
+    // $(window).scroll(function(){
+    //     var scroll = $(window).scrollTop();
 
-        if (scroll >= 200) {
-            $('.nav-bar').addClass('show-nav');
-        }
+    //     if (scroll >= 200) {
+    //         $('.nav-bar').addClass('show-nav');
+    //     }
         // else {
         //  $('.scroll-wrap').removeClass('show-nav');
         // }
 
-    });
+    // });
+
+    if ($(window).scrollTop() > 300){
+        $('.nav-bar').addClass( "show-nav");
+    }
+    else {
+        $('.nav-bar').removeClass("show-nav");
+    }
 
 
 });
