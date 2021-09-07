7ecc67cf074ef910d4db7f0aa0f2f1577d2c7307
jaredkitade <jkitade@cca.edu>
2021-09-07 14:52:35 -0700

    Update nav-bar.js

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -1,15 +1,16 @@
-$(function(){
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
-        var os = $('.logo-large').offset().top; // pixels to the top of div1
-        var ht = $('.logo-large').height(); // height of div1 in pixels
-        // if you've scrolled further than the top of div1 plus it's height
-        // change the color. either by adding a class or setting a css property
-        if(scroll > os + ht){
-            $('.nav-bar').toggleClass('reveal');
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 300) {
+            $('.nav-bar').addClass('reveal');
         }
-        // if(scroll < os + ht){
-        //     $('.nav-bar').toggleClass('reveal');
-        // }
+        else {
+            $('.nav-bar').removeClass('reveal');
+        }
+
     });
+
 });
