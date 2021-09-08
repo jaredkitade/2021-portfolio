25006004c0cdaef7f7c09fb07f74e1348093ec77
jaredkitade <jkitade@cca.edu>
2021-09-07 17:29:19 -0700

    Update nav-bar.js

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         $("#sitemap-placeholder").load("sitemap.html");
     });
 
-    $('.sitemap-p').click(function() {
+    $('.sitemap-q').click(function() {
         $(".sitemap").toggleClass("show-sitemap");
     });
 });
