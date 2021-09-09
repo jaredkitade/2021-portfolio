19adb19b0894a495fd474edd861c79275c90dfe5
jaredkitade <jkitade@cca.edu>
2021-09-08 17:44:47 -0700

    lots of stuff

--- assets/scripts/nav-bar.js
+++ assets/scripts/nav-bar.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-        $(".nav-bar").toggleClass("show-nav", ($(window).scrollTop() > 250));
+        $(".nav-bar").toggleClass("show-nav", ($(window).scrollTop() > 150));
+        $(".logo-large").toggleClass("show-nav", ($(window).scrollTop() < 150));
      });
 
     $(function(){
         $("#nav-placeholder").load("nav.html");
         $("#sitemap-placeholder").load("sitemap.html");
+        $("#footer-placeholder").load("footer.html");
     });
 
     $(".sitemap-q").click(function(){
-        $(".sitemap").toggleClass("show-sitemap");
+        $(".sitemap-container").toggleClass("show-sitemap");
+        $(".nav-bar").toggleClass("logo-behind");
     });
 
 });
